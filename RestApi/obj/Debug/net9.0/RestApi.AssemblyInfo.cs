4811041e363a498f3b141634a28b6e800b32d2eb
Hakan ÖZER <hakanozer02@gmail.com>
2026-03-27 16:26:16 +0300

    Sonar - Owasp Zap

--- RestApi/obj/Debug/net9.0/RestApi.AssemblyInfo.cs
+++ RestApi/obj/Debug/net9.0/RestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c13921a3da6b8fd3b6b236df9402e2a3322e0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97b4aaffb04d7e122f981247c8075ef7a951342")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
